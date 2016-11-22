fd9974f7ffd76ca242f07563ee2a7ff60092caf4
chadmorita <chadmmm@hawaii.edu>
2016-11-22 10:16:06 -1000

    Backup.

--- app/imports/ui/pages/calendar-page.js
+++ app/imports/ui/pages/calendar-page.js
@@ -1,79 +1,81 @@
-import { Tracker } from 'meteor/tracker';
-import { Sessions, SessionsSchema } from '../../api/sessions/sessions.js';
+import {Tracker} from 'meteor/tracker';
+import {Sessions, SessionsSchema} from '../../api/sessions/sessions.js';
 
-
-let isPast = ( date ) => {
+let isPast = (date) => {
   // Get access to today's moment
   let today = moment().format();
-  return moment( today ).isAfter( date );
+  return moment(today).isAfter(date);
 };
 
 // Subscribe to the "events" collection.
-Template.Calendar_Page.onCreated( () => {
+Template.Calendar_Page.onCreated(() => {
   let template = Template.instance();
-  template.subscribe( 'Sessions' );
+  template.subscribe('Sessions');
 });
 
-Template.Calendar_Page.onRendered( () => {
+Template.Calendar_Page.onRendered(() => {
   // Initialize the calendar.
-  $( '#sessions-calendar' ).fullCalendar({
+  $('#sessions-calendar').fullCalendar({
     // Add events to the calendar.
-    events( start, end, timezone, callback ) {
-      let data = Sessions.find().fetch().map( ( session ) => {
+    events(start, end, timezone, callback) {
+      let data = Sessions.find().fetch().map((session) => {
         // Don't allow already past study sessions to be editable.
-        session.editable = !isPast( session.start );
+        session.editable = !isPast(session.start);
         return session;
       });
 
-      if ( data ) {
-        callback( data );
+      if (data) {
+        callback(data);
       }
     },
-    eventRender( session, element ) {
-      element.find( '.fc-content' ).html(
+    eventRender(session, element) {
+      element.find('.fc-content').html(
           `<h4 class="course">${ session.course }</h4>
          <p class="topic">${ session.topic }</p>
         `
       );
     },
     // Drag and drop events.
-    eventDrop( session, delta, revert ) {
+    eventDrop(session, delta, revert) {
       let date = session.start.format();
-      if ( !isPast( date ) ) {
+      if (!isPast(date)) {
         let update = {
           _id: session._id,
           start: date,
           end: date
         };
 
-        Meteor.call( 'editEvent', update, ( error ) => {
-          if ( error ) {
-            Bert.alert( error.reason, 'danger' );
+        Meteor.call('editEvent', update, (error) => {
+          if (error) {
+            Bert.alert(error.reason, 'danger');
           }
         });
       } else {
         revert();
-        Bert.alert( 'Sorry, you can\'t move items to the past!', 'danger' );
+        Bert.alert('Sorry, you can\'t move items to the past!', 'danger');
       }
     },
     // Modal to add event when clicking on a day.
-    // dayClick( date ) {
-    //   Session.set( 'eventModal', { type: 'add', date: date.format() } );
-    //   console.log("here");
-    //   $( '#add-edit-event-modal' ).modal( 'show' );
-    // },
+    dayClick(date, session) {
+      Session.set('eventModal', { type: 'add', date: date.format() });
+      console.log("here");
+      //$( '#add-edit-event-modal' ).modal( 'show' );
+      $('.ui.modal')
+          .modal('show')
+      ;
+    },
 
     // Directs to study session detail page.
-    eventClick( event ) {
-      Session.set( 'eventModal', { type: 'edit', event: event._id } );
+    eventClick(event) {
+      Session.set('eventModal', { type: 'edit', event: event._id });
       FlowRouter.go('Study_Session_Detail_Page', { _id: event._id });
       // $( '#add-edit-event-modal' ).modal( 'show' );
     }
   });
 
   // Updates the calendar if there are changes.
-  Tracker.autorun( () => {
+  Tracker.autorun(() => {
     Sessions.find().fetch();
-    $( '#sessions-calendar' ).fullCalendar( 'refetchEvents' );
+    $('#sessions-calendar').fullCalendar('refetchEvents');
   });
 });

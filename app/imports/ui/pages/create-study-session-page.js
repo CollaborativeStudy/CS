1e7303ddb4f10bedd0fc78a5a9158a53e59727b1
Mary Santabarbara <meds@hawaii.edu>
2016-11-27 16:37:06 -1000

    Revert "cleanup of comments and console.log()"
    This reverts commit 6e5c5d8ace19b9aad8cc9f02c2465f23ea733309.

--- app/imports/ui/pages/create-study-session-page.js
+++ app/imports/ui/pages/create-study-session-page.js
@@ -1,3 +1,78 @@
+/* import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { _ } from 'meteor/underscore';
+import { Sessions, SessionsSchema } from '../../api/sessions/sessions.js'; */
+
+/* eslint-disable no-param-reassign */
+
+/* const displayErrorMessages = 'displayErrorMessages';
+
+Template.Create_Study_Session_Page.onCreated(function onCreated() {
+  this.messageFlags = new ReactiveDict();
+  this.messageFlags.set(displayErrorMessages, false);
+  this.context = SessionsSchema.namedContext('Create_Study_Session_Page');
+});
+
+Template.Create_Study_Session_Page.helpers({
+  errorClass() {
+    return Template.instance().messageFlags.get(displayErrorMessages) ? 'error' : '';
+  },
+  displayFieldError(fieldName) {
+    const errorKeys = Template.instance().context.invalidKeys();
+    return _.find(errorKeys, (keyObj) => keyObj.name === fieldName);
+  },
+});
+
+Template.Create_Study_Session_Page.onRendered(function enableSemantic() {
+  const instance = this;
+  instance.$('select.ui.dropdown').dropdown();
+  instance.$('.ui.selection.dropdown').dropdown();
+  instance.$('ui.fluid.search.dropdown').dropdown();
+});
+
+Template.Create_Study_Session_Page.events({
+  'submit .session-data-form'(event, instance) {
+    event.preventDefault();
+    // Get name (text field)
+    const name = Meteor.user().profile.name;
+    const e = document.getElementById(event.target.course.id);
+    let course = e.options[e.selectedIndex].text;
+    if (course === 'Select a Course') {
+      course = '';
+    }
+    const topic = event.target.topic.value;
+    const f = document.getElementById(event.target.startTime.id);
+    let startTime = f.options[f.selectedIndex].text;
+    if (startTime === 'Select a Start Time') {
+      startTime = '';
+    }
+    const g = document.getElementById(event.target.endTime.id);
+    let endTime = g.options[g.selectedIndex].text;
+    if (endTime === 'Select an End Time') {
+      endTime = '';
+    }
+    const startTimeV = event.target.startTime.value;
+    const endTimeV = event.target.endTime.value;
+
+    const newSession = { name, course, topic, startTime, endTime, startTimeV, endTimeV };
+    // Clear out any old validation errors.
+    instance.context.resetValidation();
+    // Invoke clean so that newSession reflects what will be inserted.
+    SessionsSchema.clean(newSession);
+    // Determine validity.
+    instance.context.validate(newSession);
+    if (instance.context.isValid()) {
+      Sessions.insert(newSession);
+      instance.messageFlags.set(displayErrorMessages, false);
+      FlowRouter.go('Public_Landing_Page');
+    } else {
+      instance.messageFlags.set(displayErrorMessages, true);
+    }
+  },
+}); */
+
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { ReactiveDict } from 'meteor/reactive-dict';
@@ -58,6 +133,7 @@ Template.Create_Study_Session_Page.events({
     const startV = event.target.start.value;
     const endV = event.target.end.value;
 
+    // const newSession = { name, course, topic, start, end, startV, endV };
     const newSession = { title, name, guests, course, topic, start, end, startV, endV };
     // Clear out any old validation errors.
     instance.context.resetValidation();
@@ -65,11 +141,14 @@ Template.Create_Study_Session_Page.events({
     SessionsSchema.clean(newSession);
     // Determine validity.
     instance.context.validate(newSession);
+    console.log('validated run');
     if (instance.context.isValid()) {
+      console.log('valid data');
       Sessions.insert(newSession);
       instance.messageFlags.set(displayErrorMessages, false);
       FlowRouter.go('Public_Landing_Page');
     } else {
+      console.log('invalid data');
       instance.messageFlags.set(displayErrorMessages, true);
     }
   },

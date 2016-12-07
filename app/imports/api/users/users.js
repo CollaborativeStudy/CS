0bd6f1cf824e6129af369a8db6026cfcd3c55f5a
chadmorita <chadmmm@hawaii.edu>
2016-12-06 16:52:59 -1000

    Created users collection.

---
+++ app/imports/api/users/users.js
@@ -0,0 +1,63 @@
+import { Mongo } from 'meteor/mongo';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+
+/* eslint-disable object-shorthand */
+
+export const Users = new Mongo.Collection('Users');
+
+// Block all database operations on the client-side.
+Events.allow({
+  insert: () => false,
+  update: () => false,
+  remove: () => false
+});
+
+Events.deny({
+  insert: () => true,
+  update: () => true,
+  remove: () => true
+});
+
+/**
+ * Create the schema for Sessions
+ */
+export const UsersSchema = new SimpleSchema({
+  //
+  name: {
+    label: 'Name',
+    type: String,
+    optional: false,
+  },
+  username: {
+    label: 'Username',
+    type: String,
+    optional: false,
+  },
+  profilePicture: {
+    label: 'Profile Picture',
+    type: String,
+    optional: false,
+  },
+  pros: {
+    label: 'Pros',
+    type: String,
+    optional: true,
+  },
+  studs: {
+    label: 'Studs',
+    type: String,
+    optional: true,
+  },
+  termsOfUse: {
+    label:'Terms of Use',
+    type: Boolean,
+    optional: false,
+  },
+  admin: {
+    label: 'Admin Permission',
+    type: Boolean,
+    optional: false,
+  }
+});
+
+Sessions.attachSchema(UsersSchema);

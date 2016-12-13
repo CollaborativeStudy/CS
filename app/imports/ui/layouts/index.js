0256d4d5033103bbbd4531bd9d79436b97978c34
mariahgaoiran <mariahgaoiran@gmail.com>
2016-12-12 22:52:07 -1000

    combine profile and home into one dropdown

--- app/imports/ui/layouts/index.js
+++ app/imports/ui/layouts/index.js
@@ -1,12 +1,15 @@
 import './app-body.html';
 import './app-body.js';
 import './app-not-found.html';
+import './cas-login.html'
+import './cas-login';
 import './footer.html';
 import './header.html';
 import './if-logged-in.html';
 import './if-logged-in.js';
 import './loading.html';
-import './cas-login.html'
-import './cas-login';
+
 import './home-dropdown.html';
 import './home-dropdown';
+import './profile-dropdown.html';
+import './profile-dropdown';

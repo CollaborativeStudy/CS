aa4943ebfa8953e801719dc2981f308dce940d03
Mary Santabarbara <meds@hawaii.edu>
2016-12-14 00:42:29 -1000

    cleaned up group.js

--- app/imports/ui/pages/group-page.js
+++ app/imports/ui/pages/group-page.js
@@ -26,14 +26,6 @@ Template.Group_Page.onCreated(function onCreated() {
 });
 
 Template.Group_Page.events({
-  // 'click .edit-pro'(event, instance){
-  //   console.log('edit pro');
-  // },
-  'click .create-session'(event, instance) {
-    $('.ui.modal.sessions-modal')
-        .modal('show')
-    ;
-  },
   'click .create-group'(event, instance) {
     $('.ui.modal.groups-modal')
         .modal('show')

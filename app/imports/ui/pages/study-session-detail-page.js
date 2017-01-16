7f7df55d5e5157902afed557c89eb4298372cb41
mariahgaoiran <mariahgaoiran@gmail.com>
2017-01-15 23:41:00 -1000

    add icon, recognizes click

--- app/imports/ui/pages/study-session-detail-page.js
+++ app/imports/ui/pages/study-session-detail-page.js
@@ -53,12 +53,12 @@ Template.Study_Session_Detail_Page.events({
         { $push: { topic: event.target.addTopic.value}  });
     FlowRouter.reload();
   },
-  'submit .remove-topic'(event){
+  'click .remove.circle.icon'(event){
     event.preventDefault();
     console.log("topic: " + event.target.removeTopic.value);
-    Sessions.update(
+    /* Sessions.update(
         { _id: FlowRouter.getParam('_id') },
-        { $pull: { topic: event.target.removeTopic.value}  });
+        { $pull: { topic: event.target.removeTopic.value}  }); */
     FlowRouter.reload();
   },
   'click .join-pro'(event){

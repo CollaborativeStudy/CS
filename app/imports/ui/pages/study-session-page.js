829663d16f5f082dc2912d2e3266b54198352490
mariahgaoiran <mariahgaoiran@gmail.com>
2017-03-06 00:23:30 -1000

    clean up, primary and secondary sort orders

--- app/imports/ui/pages/study-session-page.js
+++ app/imports/ui/pages/study-session-page.js
@@ -13,6 +13,7 @@ Template.Study_Session_Page.onCreated(function onCreated() {
     this.subscribe('Sessions');
     this.subscribe('Users');
   });
+  Session.set("sort", 0);
 });
 
 /*
@@ -46,11 +47,11 @@ Template.Study_Session_Page.helpers({
 
     console.log("sort in search: " + sort);
     if (sort === 0){
-      return Sessions.find({ $or: [ { course: new RegExp(searchValue, 'i') }, { title: new RegExp(searchValue, 'i') }, { topic: new RegExp(searchValue, 'i') } ] }, {sort: {date: 1}} );
+      return Sessions.find({ $or: [ { course: new RegExp(searchValue, 'i') }, { title: new RegExp(searchValue, 'i') }, { topic: new RegExp(searchValue, 'i') } ] }, {sort: {date: 1, start: 1, course: 1}} );
     } else if (sort === 1) {
-      return Sessions.find({ $or: [ { course: new RegExp(searchValue, 'i') }, { title: new RegExp(searchValue, 'i') }, { topic: new RegExp(searchValue, 'i') } ] }, {sort: {course: 1}} );
+      return Sessions.find({ $or: [ { course: new RegExp(searchValue, 'i') }, { title: new RegExp(searchValue, 'i') }, { topic: new RegExp(searchValue, 'i') } ] }, {sort: {course: 1, date: 1, start: 1}} );
     } else {
-      return Sessions.find({ $or: [ { course: new RegExp(searchValue, 'i') }, { title: new RegExp(searchValue, 'i') }, { topic: new RegExp(searchValue, 'i') } ] }, {sort: {course: -1}} );
+      return Sessions.find({ $or: [ { course: new RegExp(searchValue, 'i') }, { title: new RegExp(searchValue, 'i') }, { topic: new RegExp(searchValue, 'i') } ] }, {sort: {course: -1, date: 1, start: 1}} );
     }
 
     // Search the Sessions collection for any sessions with the same course, title, or topic.
@@ -72,9 +73,6 @@ Template.Study_Session_Page.helpers({
   hasTutorial(){
     return Users.findOne({ username: Meteor.user().profile.name }).tutorial;
   },
-  getSort(){
-    return sort;
-  }
 });
 
 Template.Study_Session_Page.events({
@@ -100,17 +98,13 @@ Template.Study_Session_Page.events({
     const sortItem = document.getElementById("sortItem").innerHTML;
     if(sortItem === "Date"){
       console.log("Date");
-      sort = 0;
       Session.set("sort", 0);
     } else if (sortItem === "Course Number (Low to High)") {
       console.log("Course Number (Low to High)");
-      sort = 1;
       Session.set("sort", 1);
     } else if (sortItem === "Course Number (High to Low)"){
       console.log("Course Number (High to Low)");
-      sort = 2;
       Session.set("sort", 2);
     }
-    console.log("sort after click: " + sort);
   }
 });

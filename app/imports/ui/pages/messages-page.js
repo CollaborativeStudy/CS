4d6cf6a96257babb91aa5fdc31a311eb47bec940
Mary Santabarbara <meds@hawaii.edu>
2017-04-04 23:30:01 -1000

    Revert "added scroll bar"
    This reverts commit 90970794f4e25ec705f89db5f7093c1f14cbff64.

--- app/imports/ui/pages/messages-page.js
+++ app/imports/ui/pages/messages-page.js
@@ -25,8 +25,8 @@ Template.Messages_Page.helpers({
     }
   },
   updateScroll(){
-    var element = document.getElementById('message-scrolling');
-    element.scrollTop = element.scrollHeight;
+    var element = document.getElementById('message-container');
+    element.scrollTop = element.scrollHeight + 1;
   },
   clearThis(){
     var target = document.getElementById('message');
@@ -36,11 +36,7 @@ Template.Messages_Page.helpers({
   },
   hasTutorial(){
     return Users.findOne({ username: Meteor.user().profile.name }).tutorial;
-  },
-  getUserAvatar(user) {
-    console.log(Users.findOne({ username: user }).profilePicture);
-    return Users.findOne({ username: user }).profilePicture;
-  },
+  }
 });
 
 Template.Messages_Page.events({
@@ -61,9 +57,5 @@ Template.Messages_Page.events({
 
     //clear form
     event.target.text = '';
-
-    //reset scroll
-    var element = document.getElementById('message-scrolling');
-    element.scrollTop = element.scrollHeight;
   }
 });
